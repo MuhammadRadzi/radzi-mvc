4c3b5a7a535461a18b75e3e6ce266dfa8532da03
MuhammadRadzi <nippon.pein14@gmail.com>
2026-01-12 15:35:58 +0800

    Add initial project structure with controllers, routing, and license

---
+++ app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php
+
+class About {
+   public function index($nama = 'Muhammad Radzi Saleh', $pekerjaan = 'Murid')
+   {
+      echo "Halo, nama saya $nama, saya adalah $pekerjaan";
+   }
+   public function page()
+   {
+      echo 'About/page';
+   }
+
+}
+
+
+?>
